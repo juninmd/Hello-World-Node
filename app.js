a343a9559e856b7754975328131d16557611a9cf
Antonio <antonio@engsolutions.com.br>
2017-02-06 17:23:26 -0200

    git ignore

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.send("hello World");
 });
 
-app.listen(27015, function () {
-    console.log("Aplicação Rodando - porta 27015");
+app.listen(process.env.PORT || 27015, function () {
+    console.log("Aplicação Rodando - porta " + (process.env.PORT || 27015));
 });
